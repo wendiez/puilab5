a363e6ac70c81aafc822440bf5c016a1df14bbb3
wendiez <wendy.y.chang@gmail.com>
2019-10-31 01:36:06 -0400

    fixed more bugs

--- js/a9.js
+++ js/a9.js
@@ -25,6 +25,9 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     document.getElementById('btn-clickadd').addEventListener('click', additem);
     document.getElementById('btn-clickminus').addEventListener('click', minusitem);
+    document.getElementById('cancelalldealsclick').addEventListener('click',removeElement);
+
+    
 
 
     calculatetotalquantity = function(){
@@ -50,30 +53,73 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log("T"+totalshoppingcartitem_a9 + " / "+vestquant+" / "+bottlequant);
 
 
+    function removeElement() {
+        // Removes an element from the document
+        var elementremove = document.getElementById("p1","p1copy","p2copy");
+        elementremove.parentNode.removeChild(elementremove);
+        removeElement2();
+        updatepostion();
+        //updateopa();
+
+        document.getElementById("finalpricetext").innerHTML="USD 29.98";
+        document.getElementById("finalpricetext").style.color="red";
+        document.getElementById("totalcheckouttext").innerHTML= "Total 2 items)"
+        document.getElementById("discountprice").style.visibility="hidden";
+    }
+
+    function removeElement2() {
+        // Removes an element from the document
+        var elementremovelist = document.querySelectorAll('.group4');
+        for (var i = 0; i < elementremovelist.length; i++){
+            elementremovelist[i].parentNode.removeChild(elementremovelist[i]);
+        };
+        
+    }
+
+    function updateopa(){
+        let xlist=document.querySelectorAll('.p1');
+        for (var i = 0; i < xlist.length; i++){
+            x[i].style.opcacity=0.2;
+        };
+    }
+
+    function updatepostion(){
+        document.getElementById("p1copy").style.top="361px";
+        document.getElementById("p1copy2").style.top="810px";
+        document.getElementById("group4copy").style.top="529px";
+        document.getElementById("group4copy2").style.top="978px";
+
+    }
 
     function additem(){
         console.log('add');
         //shoppingcartitem_a9 = parseInt(document.getElementById('cartItem').innerHTML);
         shoppingcartitem_a9++;
+        harnesstotalpricefixed=shoppingcartitem_a9*39.99;
         localStorage.setItem("shoppingcartitem_00",shoppingcartitem_a9);
+        document.getElementById("harnesstotalpricetxt").innerHTML = "USD"+ "<br>" +harnesstotalpricefixed;
         document.getElementById('harnessquanttext').innerHTML = shoppingcartitem_a9; //change number
-  
+        document.getElementById("cartItem-a9").innerHTML= shoppingcartitem_a9+2;
+
+        calculatefinalprice ();
         }
     
 
     function minusitem(){
         console.log('minus');
         if (shoppingcartitem_a9 <= 0){
-            
-         document.getElementsByClassName("class="product1")
-
-        }else{
-            shoppingcartitem_a9--;  
+             //document.getElementsByClassName("class="product1");
+             removeElement();
+        }else {
+            shoppingcartitem_a9--; 
+            harnesstotalpricefixed=shoppingcartitem_a9*9.99; 
         }
         localStorage.setItem("shoppingcartitem_00",shoppingcartitem_a9);
+        document.getElementById("harnesstotalpricetxt").innerHTML = "USD"+ "<br>" +harnesstotalpricefixed;
         document.getElementById('harnessquanttext').innerHTML = shoppingcartitem_a9;
-       
-        }
+        document.getElementById("cartItem-a9").innerHTML= shoppingcartitem_a9+2; 
+        calculatefinalprice ();
+           }
         
 });
 
